e51585cf32948bf719778a6ccc1e7b240fac0c03
amurfie2003 <amurfie2003@gmail.com>
2023-10-25 14:59:12 -0400

    created function to display menu options on the console

---
+++ main.py
@@ -0,0 +1,18 @@
+#display the menu options on the console
+def display_menu():
+    print("================ TODOs ===============")
+    print("1. View Task")
+    print("2. Add Task")
+    print("3. Exit")
+    print("====================================")
+    return 
+
+
+
+
+
+def main():
+    display_menu()
+    return
+
+main()
